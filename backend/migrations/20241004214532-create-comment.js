f69643ad9372e68aec3e1b9dc51a82c37ee21205
Ni Bsv <>
2024-10-05 01:05:14 +0300

    Define models (Article, Comment) with migrations

---
+++ backend/migrations/20241004214532-create-comment.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('comments_tbl', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      articleId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model : {
+            tableName: 'articles_tbl',
+          },
+        },
+        allowNull: false,
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('comments_tbl');
+  }
+};
---
+++ backend/models/comment.js
@@ -0,0 +1,22 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    static associate(models) {
+      models.Article.hasMany(Comment, {foreignKey: 'articleId'})
+    }
+  }
+  Comment.init({
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Comment',
+    tableName: 'comments_tbl'
+  });
+  return Comment;
+};
