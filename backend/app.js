0fade97ce3657e19d2ff7c803275b52fd473c490
Ni Bsv <>
2024-10-05 01:09:56 +0300

    Add database configuration

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const retry = require('async-retry');
+
+const db = require('./models')
 
 
 const env = process.env.NODE_ENV
@@ -8,6 +11,20 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+(async () => {
+    try {
+      await retry(async () => {
+        await db.sequelize.authenticate()
+            .then(() => {
+                app.listen(PORT, () => {
+                    console.log(`Server listening on port ${PORT}`);
+                });
+            })
+      }, 
+      {
+        retries: 5,
+        minTimeout: 2400,
+      });
+    } catch (err) {
+    }
+})();
---
+++ backend/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'articles_db',
+    host: process.env.POSTGRES_HOST || '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'articles_db_test',
+    host: process.env.POSTGRES_HOST || '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+  }
+};
