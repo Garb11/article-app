a090652a98e82aec8c1c12ef36d037978bf80ffd
Ni Bsv <>
2024-10-14 05:41:49 +0300

    Add header with navigation links

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,35 @@
 <template>
   <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>{{ $t("name.app_title") }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text @click="navigateToPage('articles')">{{
+        $t("name.articles_page")
+      }}</v-btn>
+    </v-app-bar>
     <v-main>
-      <router-view />
+      <v-container v-if="error">
+        <v-alert type="error" v-for="err in error" outlined>
+          {{ $t(err) }}
+        </v-alert>
+      </v-container>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from "vue-router";
+import { onMounted, onBeforeUnmount, ref } from "vue";
+import { useStore } from "vuex";
+
+const refetchTimeout = 15000;
+
+const store = useStore();
+const router = useRouter();
+
+const error = ref(undefined);
+
+const navigateToPage = (page) => router.push({ name: page });
+
+</script>
